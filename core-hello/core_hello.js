ca6ecd4226d6b489a9bb9fd747800c3a8873004a
Ryan Nguyen <xryanguyenx@gmail.com>
2019-04-28 23:11:04 -0700

    Lint core-hello

--- core-hello/core_hello.js
+++ core-hello/core_hello.js
@@ -1,4 +1,4 @@
-let template = document.createElement('template');
+let template = document.createElement('template')
 template.innerHTML = `
     <style>
         h1{
@@ -26,64 +26,64 @@ template.innerHTML = `
         }
     </style>
     <h1><span id="hello"></span> <slot></slot></h1>
-`;
+`
 
 // core-hello class definition
 class CoreHello extends HTMLElement {
-    // Rainbow attribute
-    get rainbow(){
-        return this.hasAttribute('rainbow');
-    }
+  // Rainbow attribute
+  get rainbow () {
+    return this.hasAttribute('rainbow')
+  }
 
-    set rainbow(val) {
-        if (val) {
-            this.setAttribute('rainbow', '');
-        } else {
-            this.removeAttribute('rainbow');
-        }
-        this.toggleRainbow();
+  set rainbow (val) {
+    if (val) {
+      this.setAttribute('rainbow', '')
+    } else {
+      this.removeAttribute('rainbow')
     }
+    this.toggleRainbow()
+  }
 
-    // Lang attribute
-    get lang(){
-        return this.hasAttribute('rainbow');
+  // Lang attribute
+  get lang () {
+    return this.hasAttribute('rainbow')
+  }
+
+  set lang (val) {
+    this.setAttribute('lang', val)
+  }
+
+  constructor () {
+    super()
+
+    // Attach shadow root
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+    shadowRoot.appendChild(template.content.cloneNode(true))
+
+    // Place holder for message in specified language
+    this.language = shadowRoot.querySelector('#hello')
+  }
+
+  connectedCallback () {
+    // Default language
+    if (!this.hasAttribute('lang')) {
+      this.setAttribute('lang', 'en')
     }
+  }
 
-    set lang(val) {
-        this.setAttribute('lang', val);
+  static get observedAttributes () {
+    return ['lang']
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    if (this.getAttribute('lang') == 'en') {
+      this.language.innerText = 'Hello World'
+    } else if (this.getAttribute('lang') == 'es') {
+      this.language.innerText = 'Hola Mundo'
+    } else if (this.getAttribute('lang') == 'ko') {
+      this.language.innerText = '안녕하세요 세계'
     }
-
-    constructor() {
-        super();
-
-        // Attach shadow root
-        const shadowRoot = this.attachShadow({mode: 'open'});
-        shadowRoot.appendChild(template.content.cloneNode(true))
-
-        // Place holder for message in specified language
-        this.language = shadowRoot.querySelector('#hello');
-    }
-
-    connectedCallback() {
-        // Default language
-        if (!this.hasAttribute('lang')) {
-            this.setAttribute('lang', 'en');
-        }
-    }
-
-    static get observedAttributes() {
-        return ['lang'];
-    }
-  
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (this.getAttribute('lang') == 'en'){
-            this.language.innerText = 'Hello World';
-        } else if (this.getAttribute('lang') == 'es') {
-            this.language.innerText = 'Hola Mundo';
-        } else if (this.getAttribute('lang') == 'ko') {
-            this.language.innerText = '안녕하세요 세계';
-        }
-    }
+  }
 }
 
-window.customElements.define('core-hello', CoreHello);
+window.customElements.define('core-hello', CoreHello)
