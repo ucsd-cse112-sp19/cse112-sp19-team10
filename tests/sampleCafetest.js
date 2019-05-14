63bd6a77c180f6076a1b0992390a37e381ad99cf
Tim <ptcheng@ucsd.ede>
2019-05-14 11:18:56 -0700

    testcafe integration @ added lint ignore on tests/ file

--- tests/sampleCafetest.js
+++ tests/sampleCafetest.js
@@ -1,12 +1,12 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
-fixture `Getting Started`
-    .page `http://devexpress.github.io/testcafe/example`;
-    //to use local file look at this https://devexpress.github.io/testcafe/documentation/test-api/test-code-structure.html#specifying-the-start-webpage
-    //Eg fixture `MyFixture`
-    //  .page `file:///user/my-website/index.html`;
-    //
+fixture`Getting Started`
+  .page`http://devexpress.github.io/testcafe/example`
+// to use local file look at this https://devexpress.github.io/testcafe/documentation/test-api/test-code-structure.html#specifying-the-start-webpage
+// Eg fixture `MyFixture`
+//  .page `file:///user/my-website/index.html`;
+//
 
 test('My first test', async t => {
-    // Test code empty for now
-});
+  // Test code empty for now
+})
