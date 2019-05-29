ba8c4032118d3e03d7f729998174dfc338ce4b32
Ryan Nguyen <xryanguyenx@gmail.com>
2019-05-28 20:40:04 -0700

    Tooltip CSS Example

--- src/core_tooltip.js
+++ src/core_tooltip.js
@@ -1,7 +1,56 @@
 let template = document.createElement('template')
 template.innerHTML = ` 
-    <style>
-    </style>
+<style>
+    /* Tooltip container */
+    .tooltip {
+        position: relative;
+        display: inline-block;
+    }
+
+    /* Tooltip text */
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        background-color: #303133;
+        color: #fff;
+        font-family: Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,SimSun,sans-serif;
+        font-size: 12px;
+        text-align: center;
+        word-wrap: break-word;
+        line-height: 1.2;
+        padding: 10px;
+        border-radius: 4px;
+
+        /* Position the tooltip text*/
+        position: absolute;
+        z-index: 1;
+    
+        /* Position the tooltip text with arrow */
+        /*bottom: 125%;
+        left: 50%;
+        margin-left: -60px;*/
+    }
+    
+    /* Tooltip arrow */
+    .tooltip .tooltiptext::after {
+        content: "";
+        position: absolute;
+        /*top: 100%;
+        left: 50%;
+        margin-left: -5px;*/
+        border-width: 5px;
+        border-style: solid;
+        border-color: #555 transparent transparent transparent;
+    }
+
+    /* Show the tooltip text when you mouse over the tooltip container */
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+    }
+</style>
+<div class="tooltip">
+    <slot></slot>
+    <span class="tooltiptext">Text</span>
+</div>
 `
 class CoreTooltip extends window.HTMLElement {
   constructor () {
