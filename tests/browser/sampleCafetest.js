6e20730cf9501c43a41a95174d31701b19964a0c
Sean Nam <namsangwoo1@gmail.com>
2019-06-11 14:48:18 -0700

    Moved tests into subfolders

---
+++ tests/browser/sampleCafetest.js
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe'
+
+fixture`core hello browser test:`
+  .page`../examples/hello.html`
+// to use local file look at this https://devexpress.github.io/testcafe/documentation/test-api/test-code-structure.html#specifying-the-start-webpage
+// Eg fixture `MyFixture`
+//  .page `file:///user/my-website/index.html`;
+//
+
+test('My first test @added core-hello test', async t => {
+  await t
+       // Use the assertion to check the first core-hello with id = test_cafe
+        .expect(Selector('#test_cafe').innerText).eql('Thomas');
+})
