76cfa1ad01bfa481ad6a6b0f58ee04534aca9652
Ryan Nguyen <xryanguyenx@gmail.com>
2019-06-13 18:35:56 -0700

    minimum rate changes

--- src/core_rate.js
+++ src/core_rate.js
@@ -281,6 +281,7 @@ class CoreRate extends window.HTMLElement {
   set disabledVoidIconClass (val) {
     this.setAttribute('disabled-void-icon-class', val)
   }
+  
   /**
   * This function gets the value of the show-text attribute.
   * @returns {Boolean} whether to display texts.
